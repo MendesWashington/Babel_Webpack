47cd093132dadb6c2c57facfffe19b1e3ba0c968
mendesswashington@gmail.com <mendesswashington@gmail.com>
2021-10-25 17:17:59 -0300

    Adicionado o server do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,36 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "bundle.js"
+        filename: "bundle.js",
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx'],
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 3000,
+
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        })
+    ],
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use:'babel-loader'
+                use: 'babel-loader',
             }
         ]
     }
